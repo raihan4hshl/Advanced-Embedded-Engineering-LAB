332a20b65620bfd323a6a7a43609697ffb4c711e
raihan4hshl <raihan.uddin4hshl@gmail.com>
2025-04-22 23:34:25 +0200

    the .ino file is well arranged

--- Lab_01Task_controlLED/LED_on_off_webServer/LED_on_off_webServer.ino
+++ Lab_01Task_controlLED/LED_on_off_webServer/LED_on_off_webServer.ino
@@ -1,37 +1,9 @@
-/*
-
-  WiFi Web Server LED Blink
-
-  A simple web server that lets you blink an LED via the web.
-
-  This sketch will create a new access point (with no password).
-
-  It will then launch a new server and print out the IP address
-
-  to the Serial monitor. From there, you can open that address in a web browser
-
-  to turn on and off the LED on pin 13.
-
-  If the IP address of your board is yourAddress:
-
-    http://yourAddress/H turns the LED on
-
-    http://yourAddress/L turns it off
-
-  created 25 Nov 2012
-
-  by Tom Igoe
-
-  adapted to WiFi AP by Adafruit
-
- */
-
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include "arduino_secrets.h"
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+char ssid[] = SECRET_SSID;        
+char pass[] = SECRET_PASS;    
 int keyIndex = 0;                // your network key Index number (needed only for WEP)
 
 int led =  LED_BUILTIN;
@@ -39,113 +11,56 @@ int status = WL_IDLE_STATUS;
 
 WiFiServer server(80);
 
-void setup() {
-
-  //Initialize serial and wait for port to open:
-
+void setup() 
+{
   Serial.begin(9600);
-
-  while (!Serial) {
-
+  while (!Serial) 
+  {
     ; // wait for serial port to connect. Needed for native USB port only
-
   }
-
   Serial.println("Access Point Web Server");
-
-  pinMode(led, OUTPUT);      // set the LED pin mode
-
-  // check for the WiFi module:
-
-  if (WiFi.status() == WL_NO_MODULE) {
-
+  pinMode(led, OUTPUT);     
+  if (WiFi.status() == WL_NO_MODULE) 
+  {
     Serial.println("Communication with WiFi module failed!");
-
-    // don't continue
-
     while (true);
-
   }
-
   String fv = WiFi.firmwareVersion();
-
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) 
+  {
     Serial.println("Please upgrade the firmware");
-
   }
-
-  // by default the local IP address of will be 192.168.4.1
-
-  // you can override it with the following:
-
-  // WiFi.config(IPAddress(10, 0, 0, 1));
-
-  // print the network name (SSID);
-
   Serial.print("Creating access point named: ");
-
   Serial.println(ssid);
-
-  // Create open network. Change this line if you want to create an WEP network:
-
   status = WiFi.beginAP(ssid, pass);
-
-  if (status != WL_AP_LISTENING) {
-
+  if (status != WL_AP_LISTENING) 
+  {
     Serial.println("Creating access point failed");
-
-    // don't continue
-
     while (true);
-
   }
-
-  // wait 10 seconds for connection:
-
   delay(10000);
-
-  // start the web server on port 80
-
   server.begin();
-
-  // you're connected now, so print out the status
-
   printWiFiStatus();
 }
 
-void loop() {
-
-  // compare the previous status to the current status
-
-  if (status != WiFi.status()) {
-
-    // it has changed update the variable
-
+void loop() 
+{
+  if (status != WiFi.status()) 
+  {
     status = WiFi.status();
-
-    if (status == WL_AP_CONNECTED) {
-
-      // a device has connected to the AP
-
+    if (status == WL_AP_CONNECTED) 
+    {
       Serial.println("Device connected to AP");
-
-    } else {
-
-      // a device has disconnected from the AP, and we are back in listening mode
-
+    } 
+    else 
+    {
       Serial.println("Device disconnected from AP");
-
     }
-
   }
+  WiFiClient client = server.available(); 
 
-
-
-  WiFiClient client = server.available();   // listen for incoming clients
-
-  if (client) {                             // if you get a client,
-
+  if (client) 
+  {                             
     Serial.println("new client");           // print a message out the serial port
 
     String currentLine = "";                // make a String to hold incoming data from the client
